311905789bc7974e8f2cfad657a5e5409efd8df6
Sunli <scott_s829@163.com>
2021-05-24 20:54:15 +0800

    Update window.rs

--- libs/core/src/planner/window.rs
+++ libs/core/src/planner/window.rs
@@ -1,9 +1,59 @@
+use chrono::TimeZone;
+use chrono::{DateTime, Datelike, Duration, NaiveDate};
+use chrono_tz::Tz;
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
+pub enum Period {
+    Day,
+    Week,
+    Month,
+    Year,
+}
+
+impl Period {
+    pub fn window(&self, time: DateTime<Tz>) -> (DateTime<Tz>, DateTime<Tz>) {
+        let start = match self {
+            Period::Day => time.date().and_hms(0, 0, 0),
+            Period::Week => {
+                (time.date() - Duration::days(time.date().weekday() as i64)).and_hms(0, 0, 0)
+            }
+            Period::Month => time.date().with_day(1).unwrap().and_hms(0, 0, 0),
+            Period::Year => time
+                .date()
+                .with_month(1)
+                .unwrap()
+                .with_day(1)
+                .unwrap()
+                .and_hms(0, 0, 0),
+        };
+
+        let end = match self {
+            Period::Day => start + Duration::days(1),
+            Period::Week => start + Duration::days(7),
+            Period::Month => {
+                if start.month() < 12 {
+                    start.with_month(start.month() + 1).unwrap()
+                } else {
+                    start
+                        .with_year(start.year() + 1)
+                        .and_then(|t| t.with_month(1))
+                        .unwrap()
+                }
+            }
+            Period::Year => start.with_year(start.year() + 1).unwrap(),
+        };
+
+        (start, end)
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Window {
     Fixed { length: i64 },
     Sliding { length: i64, interval: i64 },
+    Period { period: Period, timezone: Tz },
 }
 
 impl Window {
@@ -22,6 +72,11 @@ impl Window {
                     time += interval;
                 }
                 windows
+            }
+            Window::Period { period, timezone } => {
+                let datetime = timezone.timestamp_millis(timestamp);
+                let (start, end) = period.window(datetime);
+                vec![(start.timestamp_millis(), end.timestamp_millis())]
             }
         }
     }
